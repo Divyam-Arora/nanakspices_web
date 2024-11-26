8d1827f2db45aa170c86f6c97ef6c113a7132132
Divyam Arora <divyamk.a.83@gmail.com>
2024-11-27 00:00:45 +0530

    sudo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center w-full">
+    <div className="flex flex-col justify-center items-center w-full">
       <p className="text-xs">
         powered by <span className="font-bold">Sunil Industries</span>{" "}
         &copy;2024
+      </p>
+      <p className="text-xs">
+        contact{" "}
+        <Link href={"mailto:mka1408@gmail.com"}>
+          <span className="font-bold">mka1408@gmail.com</span>
+        </Link>
       </p>
     </div>
   );
