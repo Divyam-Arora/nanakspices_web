0a886863641a609d08a8d5447e6e14bbd1938669
Divyam Arora <divyamk.a.83@gmail.com>
2024-11-26 23:46:11 +0530

    initial

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center w-full">
+      <p className="text-xs">
+        powered by <span className="font-bold">Sunil Industries</span>{" "}
+        &copy;2024
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
