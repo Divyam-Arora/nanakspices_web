81087bb1b10b55ebc2a4d7b5922d9b7ed9edc5f3
Divyam Arora <divyamk.a.83@gmail.com>
2024-11-27 05:05:11 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-2 p-4 flex-1">
-      <h1 className="text-8xl text-gray-800 drop-shadow-lg">Nanak Spices</h1>
+      <h1 className="text-8xl text-gray-800 drop-shadow-lg text-center">
+        Nanak Spices
+      </h1>
       <p className="text-slate-500 animate-pulse">coming soon..</p>
     </div>
   );
